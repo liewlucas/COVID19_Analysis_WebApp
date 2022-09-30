4b841e86437afad1b0c16d6c6995e6641a67d3ea
lucasliew <lucasliew88@gmail.com>
2022-09-30 17:36:24 +0800

    added streamlit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #INF1002 Python Project Group 7
-import pandas
+
+#pandas for data processing
+import pandas as pd
+
+#streamlit for gui
+import streamlit as st
+
 
 data = pandas.read_csv('covid-19-daily-figures.csv')
 print(data)
