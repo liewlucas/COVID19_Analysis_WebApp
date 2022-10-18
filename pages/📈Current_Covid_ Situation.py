333d1b182bf8d6a69206ad700ff84aea294205a2
thadd <thaddaeuskwok1@gmail.com>
2022-10-18 15:06:53 +0800

    Added summary of vaccine efficacy

--- pages/📈Current_Covid_ Situation.py
+++ pages/📈Current_Covid_ Situation.py
@@ -4,25 +4,34 @@ import requests
 from streamlit_lottie import st_lottie
 
 
-#functions to retrieve the following numbers and format case numbers with "," as the thousand seperator
+# functions to retrieve the following numbers
 def total_cases(country):
-    return f'{int(df_covid_cases.loc[country]["total_cases"]):,}'
+    return int(df_covid_cases.loc[country]["total_cases"])
+
 
 def total_cases_per_mil(country):
-    return f'{int(df_covid_cases.loc[country]["total_cases_per_million"]):,}'
+    return int(df_covid_cases.loc[country]["total_cases_per_million"])
+
 
 def new_cases(country):
-    return f'{int(df_covid_cases.loc[country]["new_cases"]):,}'
+    return int(df_covid_cases.loc[country]["new_cases"])
+
 
 def new_cases_per_mil(country):
-    return f'{int(df_covid_cases.loc[country]["new_cases_per_million"]):,}'
+    return int(df_covid_cases.loc[country]["new_cases_per_million"])
+
+
+def new_cases_7d(country):
+    return int(df_covid_cases.loc[country]["new_cases_smoothed"])
+
 
 def last_updated(country):
     return df_covid_cases.loc[country]["last_updated_date"]
 
+
 # function to load animation
 def loadlottie(url):
-    #get url
+    # get url
     r = requests.get(url)
 
     # if returns error code 200, return none, else return json/ animation file
@@ -30,10 +39,26 @@ def loadlottie(url):
         return None
     return r.json()
 
-# function to convert df into csv for exporting
-@st.cache
-def convert_to_csv(df):
-    return df.to_csv(header=True, index=True).encode('utf-8')
+
+# stable url for latest data in csv format
+covid_cases_url = "https://covid.ourworldindata.org/data/latest/owid-covid-latest.csv"
+
+# vaccine data
+df_vax_efficacy_summary = pd.read_csv("vaccine_efficacy_summary.csv", skiprows=1)
+df_vax_efficacy_summary.loc[len(df_vax_efficacy_summary.index)] = ["Not Vaccinated", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# df_not_vaccinated = pd.DataFrame({"Not Vaccinated": [0,0,0,0,0,0,0,0,0,0,0,0]})
+# df_vax_merge = pd.merge(df_vax_efficacy_summary, df_not_vaccinated,how='right')
+# df_not_vaccinated = pd.DataFrame({"Not Vaccinated": [0,0,0,0,0,0,0,0,0,0,0,0]})
+vaccine_types = df_vax_efficacy_summary["vaccine"].values.tolist()
+df_vax_efficacy_summary.set_index("vaccine", inplace=True)
+
+
+# create dataframes for data to be used
+df_covid_cases = pd.read_csv(covid_cases_url)
+df_country = df_covid_cases["location"]
+
+# Setting country names as index in dataframe
+df_covid_cases.set_index("location", inplace=True)
 
 
 # page start
@@ -48,37 +73,34 @@ with animation:
     st_lottie(animation2, height=280)
 
 
-# stable url for latest data in csv format
-covid_cases_url = "https://covid.ourworldindata.org/data/latest/owid-covid-latest.csv"
+# user to select country and vaccine they took as input
+country, vaccine = st.columns(2)
+with country:
+    covid_country = st.selectbox(
+        "Select a country to view the current Covid-19 situation there",
+        options=df_country,
+    )
+with vaccine:
+    user_vaccine = st.selectbox(
+        "Please indicate the vaccine that you took. If unvaccinated, select \"Not Vaccinated\".",
+        options=vaccine_types
+    )
 
-
-# create dataframes for data to be used
-df_covid_cases = pd.read_csv(covid_cases_url)
-df_country = df_covid_cases["location"]
-
-
-# Setting country names as index in dataframe
-df_covid_cases.set_index("location", inplace=True)
-
-
-# user to select country as input
-covid_country = st.selectbox(
-    "Select a country to view the current Covid-19 situation there",
-    options=df_country,
-)
 
 st.markdown("---")
 
 
-data, new_case = st.columns([2,1])
+data, new_case = st.columns([2, 1])
 with data:
     st.header(f"Covid-19 in {covid_country}")
     st.markdown(
         f"""
-        {covid_country} has a total of {total_cases(covid_country)} cases.\n
+        {covid_country} has a total of {total_cases(covid_country):,} cases.\n
         As population varies across countries, it can be insightful to compare the number as \"per million people\".\
-        There are approxiamtely {total_cases_per_mil(covid_country)} total cases per one million people and
-        {new_cases_per_mil(covid_country)} new cases per one million people.
+        There are approximately {total_cases_per_mil(covid_country):,} total cases per one million people and
+        {new_cases_per_mil(covid_country):,} new cases per one million people. \n
+        The 7-day average is also a good reference number to account for sudden spikes in cases. The 7-day average \
+        of new cases in {covid_country} is {new_cases_7d(covid_country):,}.
         """
     )
     st.markdown(
@@ -90,7 +112,7 @@ with data:
 with new_case:
     st.markdown(
         f"""<b style="font-size:130px;">
-        {new_cases(covid_country)}</b>""",
+        {new_cases(covid_country):,}</b>""",
         unsafe_allow_html=True
     )
     st.markdown(
@@ -99,4 +121,123 @@ with new_case:
         unsafe_allow_html=True
     )
 
+
 st.markdown("---")
+
+# -- FACTOR 1: VACCINATION COVERAGE RISK --
+# remove last row (Not Vaccinated) from calculation and find the average efficacy of all vaccines combined
+df_vax_efficacy_values = df_vax_efficacy_summary.copy()[:-1]
+vax_avg_efficacy = df_vax_efficacy_values.mean().mean()
+
+# recommended vaccination coverage based on vaccine efficacy
+# to eliminate peak, 100% coverage for 60% efficacy, 75% coverage for 70% efficacy and 60% coverage and 80% efficacy
+if vax_avg_efficacy >= 70:
+    vax_efficacy_coefficient = (75-60)/(80-70)
+    rec_vax_coverage = round(vax_efficacy_coefficient * (vax_avg_efficacy-70)+70)
+elif vax_avg_efficacy >= 60:
+    vax_efficacy_coefficient = (70-60)/(100-75)
+    rec_vax_coverage = round(vax_efficacy_coefficient*(vax_avg_efficacy-60)+60)
+else:
+    # vaccine efficacy too low, vaccination will not be able to do much, exclude from calculations
+    rec_vax_coverage = 0
+
+# vaccination coverage in specified country
+country_vax_coverage = int(df_covid_cases.loc[covid_country]["people_fully_vaccinated_per_hundred"])
+
+# check country's vaccination coverage against recommended coverage and assign a coverage risk score
+if country_vax_coverage > rec_vax_coverage:
+    coverage_risk = 1
+elif country_vax_coverage == rec_vax_coverage:
+    coverage_risk = 3
+else:
+    coverage_risk = 5
+
+
+# -- FACTOR 2: NEW CASES PER ONE MILLION RISK --
+# using 100 and 60 new cases per million as benchmark, assign a new case risk score
+if int(new_cases_per_mil(covid_country)) > 100:
+    new_case_risk = 5
+elif int(new_cases_per_mil(covid_country)) >= 60:
+    new_case_risk = 3
+else:
+    new_case_risk = 1
+
+
+# -- FACTOR 3: USER VACCINATION RISK --
+# assign a risk score based on user's specified vaccine efficacy against the country's vaccination coverage
+df_indv_vax_eff_avg = df_vax_efficacy_summary.mean(axis=1)
+user_vaccine_efficacy = df_indv_vax_eff_avg.loc[user_vaccine]
+
+# recommended vaccination efficacy based on vaccine coverage
+# to eliminate peak, 100% coverage for 60% efficacy, 75% coverage for 70% efficacy and 60% coverage and 80% efficacy
+if country_vax_coverage > 75:
+    vax_coverage_coefficient = (80 - 70)/(75 - 60)
+    rec_vax_efficacy = vax_coverage_coefficient * (country_vax_coverage-75)+75
+elif country_vax_coverage >= 60:
+    vax_coverage_coefficient = (100 - 75)/(70 - 60)
+    rec_vax_efficacy = vax_coverage_coefficient*(country_vax_coverage-60)+60
+else:
+    # coverage too low, vaccination will not be able to help much, exclude from calculations
+    rec_vax_efficacy = 0
+
+# check country's vaccination coverage against recommended coverage and assign a coverage risk score
+if user_vaccine_efficacy == "Not Vaccinated":
+    vax_efficacy_risk = 5
+elif user_vaccine_efficacy > rec_vax_efficacy:
+    vax_efficacy_risk = 1
+elif user_vaccine_efficacy == rec_vax_efficacy:
+    vax_efficacy_risk = 3
+else:
+    vax_efficacy_risk = 5
+
+
+# risk score
+if rec_vax_coverage == 0 and rec_vax_efficacy == 0:
+    risk_score = round(new_case_risk, 2)
+elif rec_vax_coverage == 0 or rec_vax_efficacy == 0:
+    risk_score = round((new_case_risk + coverage_risk + vax_efficacy_risk)/2, 2)
+else:
+    risk_score = round((new_case_risk + coverage_risk + vax_efficacy_risk)/3, 2)
+
+
+# determine risk level:
+if risk_score > 10:
+    risk_level = "High"
+elif risk_score > 5:
+    risk_level = "Some"
+else:
+    risk_level = "Low"
+
+
+risk, info = st.columns([1, 2])
+with risk:
+    st.markdown(
+        f"""<b right style="font-size:130px;text-align: right;">
+        {risk_level}</b>""",
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        f"""<p style="font-size:50px;line-height:0px;">
+        risks</p>""",
+        unsafe_allow_html=True
+    )
+
+with info:
+    st.header(f"Covid-19 risks")
+    st.markdown(
+        f"""
+        It is good to understand one's risk of travelling, especially during the pandemic. We have analysed and \
+        categorised each country to different risk levels (i.e. Low risk, Some risks, High risks) . \n 
+        
+        Our risk analysis is based on on 3 factors - Vaccination coverage, New cases per one million people, and the \
+        Efficacy of your vaccine. Each factor is given equal weightage and a final score is computed. The score is \
+        then compared against a scale of \'1\' to \'15\' with \'1\' being the lowest risk and \'15\' being the highest \
+        risk. Our risk score for {covid_country} is currently at {risk_score}
+        """
+    )
+    st.markdown(
+        f"""<p style="font-size:12px;">
+        *All calculations provided are just an estimate for your reference. Factors where we do not have data are \
+        excluded from our calculations.</p>""",
+        unsafe_allow_html=True
+    )
